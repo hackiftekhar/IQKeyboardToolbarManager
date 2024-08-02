aa4e7871703c6bd6ae517d1714faf9e21d7570f0
hackiftekhar <hack.iftekhar@gmail.com>
2024-08-02 13:14:10 +0530

    - Updated IQKeyboardToolbar to fix a placeholder issue

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardToolbar.git", from: "1.0.3"),
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardToolbar.git", from: "1.0.4"),
         .package(url: "https://github.com/hackiftekhar/IQTextInputViewNotification", from: "1.0.3"),
     ],
     targets: [
